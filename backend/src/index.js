2a4df7f7f710b0b8ec50f3f21f83668a5cbf6b12
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2025-08-22 19:27:23 +0530

    feat: implement MongoDB connection and refactor server startup logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+const connectToDB = require("./config/database");
 require("dotenv").config();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectToDB(process.env.MONGO_URI);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+};
+
+startServer();
